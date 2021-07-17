4942418207e4a9fba8e3c2fc6a01d809fa3c359b
Eddie Bauer <63851399+Eddie1567@users.noreply.github.com>
2021-07-18 04:08:18 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# Hackathon_agoraSDk
+# awesome-agora-webrtc
+> Go to https://agoran-association.github.io/awesome-agora-webrtc/#/ for a try.
+
+## What's in
+- [x] Basic RTC - Implement basic rtc with advanced options, such as attendee mode, codec, mode, etc.
+- [x] Video Processing - Process agora video stream with webassembly.
+- [ ] Screen Sharing - Implement screen sharing on Chrome (temporarily by Chrome extension) and Firefox.
+- [ ] Audio Mixing - Add bgm for you!
+- [ ] Custom Stream Player - Custom your own declarative stream player.
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Run your tests
+```
+npm run test
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span class="font-weight-light">Awesome Agora</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-menu offset-y>
+        <v-btn flat icon slot="activator">
+          <v-icon>menu</v-icon>
+        </v-btn>
+        <v-list>
+          <v-list-tile
+            v-for="({text, href}) in _routes"
+            :key="href"
+          >
+            <v-list-tile-title @click="$router.push(href)" style="cursor: pointer">{{ text }}</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
+    </v-toolbar>
+
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      //
+    };
+  },
+  computed: {
+    _routes() {
+      return this.$router.options.routes.map(item => ({
+        href: item.path,
+        text: item.name
+      }));
+    }
+  },
+  mounted() {
+    this.$nextTick(() => {});
+  }
+};
+</script>
